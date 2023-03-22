f65cafb8549c9866cf9462fb662e6cad4371b265
aksam103m <127625260+aksam103m@users.noreply.github.com>
2023-03-22 23:22:59 +0300

    Add files via upload

---
+++ KUZEYYEL--main/KuzeyYeli/KuzeyYeli.ORM/Entity/Urun.cs
@@ -0,0 +1,33 @@
+﻿//using KuzeyYeli.ORM.Facade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuzeyYeli.ORM.Entity
+{
+    public class Urun
+    {
+        //sadece property tanımlamada kullandık
+        public int UrunId { get; set; }
+
+        public string UrunAdi { get; set; }
+        
+        public decimal Fiyat { get; set;}//db money
+
+        public short Stok { get; set; }//db smallint
+
+        public int KategoriID { get; set; }
+
+        public int TedarikciID { get; set; }
+
+        public string BirimdekiMiktar { get; set; }
+
+        public short YeniSatis { get; set; }
+        
+        public short EnAzYenidenStisMiktari { get; set; }
+
+        public bool Sonlandi { get; set; }
+    }
+}
---
+++ KUZEYYEL--main/KuzeyYeli/KuzeyYeli.ORM/Facade/Urunler.cs
@@ -0,0 +1,51 @@
+﻿using KuzeyYeli.ORM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuzeyYeli.ORM.Facade
+{
+    public class Urunler
+    {
+        //sql ile baglantı burada saglancak
+
+        public static DataTable Listele()
+        { 
+            return Tools.Listele("UrunListele"); 
+        }
+        public static bool Ekle(Urun entity)
+        {
+            SqlCommand komut = new SqlCommand("UrunEkle", Tools.Baglanti);
+            komut.CommandType = CommandType.StoredProcedure;
+            komut.Parameters.AddWithValue("@urunAdi", entity.UrunAdi);
+            komut.Parameters.AddWithValue("@fiyat", entity.Fiyat);
+            komut.Parameters.AddWithValue("@stok", entity.Stok);
+            return Tools.ExecuteNonQuery(komut);
+        }
+
+        public static bool Sil(Urun entity)
+        {
+            SqlCommand komut = new SqlCommand("UrunSil", Tools.Baglanti);
+            komut.CommandType = CommandType.StoredProcedure;
+            komut.Parameters.AddWithValue("@urunTd", entity.UrunId);
+            return Tools.ExecuteNonQuery(komut);
+
+        }
+
+        public static bool Guncelle(Urun entity)
+        {
+            SqlCommand komut = new SqlCommand("UrunGuncelle", Tools.Baglanti);
+            komut.CommandType = CommandType.StoredProcedure;
+            komut.Parameters.AddWithValue("@a", entity.UrunAdi);
+            komut.Parameters.AddWithValue("@f", entity.Fiyat);
+            komut.Parameters.AddWithValue("@s", entity.Stok);
+            komut.Parameters.AddWithValue("@id", entity.UrunId);
+            return Tools.ExecuteNonQuery(komut);
+
+        }
+    }
+}
